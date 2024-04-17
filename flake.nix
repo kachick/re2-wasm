bba336f6b958ad7dca914605ce36693b252b1d2f
Kenichi Kamiya <kachick1@gmail.com>
2024-04-17 15:37:57 +0900

    chore: use newer emcc

--- flake.nix
+++ flake.nix
@@ -6,13 +6,15 @@
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, edge-nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
       in {
         devShells.default = with pkgs;
           mkShell {
@@ -23,7 +25,7 @@
 
               nodejs_20
               gnumake
-              emscripten # `emcc`
+              edge-pkgs.emscripten # `emcc`
             ];
           };
       });
