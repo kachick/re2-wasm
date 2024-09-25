bcc5df37de3946c5416747d11776aa67ba3f9136
Kenichi Kamiya <kachick1@gmail.com>
2024-09-25 12:06:51 +0900

    Remove needless flake-utils

--- flake.nix
+++ flake.nix
@@ -1,30 +1,35 @@
 {
   inputs = {
-    # Candidate channels
-    #   - https://github.com/kachick/anylang-template/issues/17
-    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              # https://github.com/kachick/dotfiles/pull/228
-              bashInteractive
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                bashInteractive
+                nixfmt-rfc-style
 
-              nodejs_20
-              gnumake
-              emscripten # `emcc`
-            ];
-          };
-      });
+                nodejs_20
+                gnumake
+                emscripten # `emcc`
+              ];
+            };
+        }
+      );
+    };
 }
