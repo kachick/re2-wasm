1ba3d55a697442b9ab9664d3540f2c5b0c932fc2
Kenichi Kamiya <kachick1@gmail.com>
2024-09-25 16:16:27 +0900

    Specify versions

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     {
       self,
       nixpkgs,
-      # edge-nixpkgs,
+    # edge-nixpkgs,
     }:
     let
       inherit (nixpkgs) lib;
@@ -20,8 +20,8 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          # edge-pkgs = edge-nixpkgs.legacyPackages.${system};
         in
+        # edge-pkgs = edge-nixpkgs.legacyPackages.${system};
         {
           default =
             with pkgs;
@@ -32,8 +32,10 @@
 
                 nodejs_20
                 cmake
-                abseil-cpp # https://github.com/google/re2/blob/b84e3ff189980a33d4a0c6fa1201aa0b3b8bab4a/README#L13
+                # abseil-cpp # https://github.com/google/re2/blob/b84e3ff189980a33d4a0c6fa1201aa0b3b8bab4a/README#L13
+                (abseil-cpp_202401.override { cxxStandard = "17"; })
                 ninja
+                llvmPackages_17.clang-tools
                 emscripten # `emcc`
               ];
               # LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.abseil-cpp ];
